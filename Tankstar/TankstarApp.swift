3c77c242b1470ce13a8bb9a89c54a762947afdfa
Hennes617 <hennesbolte@protonmail.com>
2025-06-07 09:47:13 +0200

    Initial Commit

---
+++ Tankstar/TankstarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TankstarApp.swift
+//  Tankstar
+//
+//  Created by Hennes Bolte on 07.06.25.
+//
+
+import SwiftUI
+
+@main
+struct TankstarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TankstarTests/TankstarTests.swift
@@ -0,0 +1,17 @@
+//
+//  TankstarTests.swift
+//  TankstarTests
+//
+//  Created by Hennes Bolte on 07.06.25.
+//
+
+import Testing
+@testable import Tankstar
+
+struct TankstarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
